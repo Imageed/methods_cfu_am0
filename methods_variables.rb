e4e5e1db72c4f41d22a893a00d96f9c7911f2c5e
Imageed <111616894+Imageed@users.noreply.github.com>
2022-08-30 15:08:11 -0600

    Update methods_variables.rb

--- methods_variables.rb
+++ methods_variables.rb
@@ -8,7 +8,11 @@
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
-
+chicken = "a large bird"
+chicken.upcase 
+chicken.downcase 
+chicken.reverse 
+chicken.length
 
 
 
@@ -23,10 +27,21 @@ last_login = "12/09/2021"
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
+user_name.start_with? ("coco")
+#this method checks to see if the variable starts with the argument
+
+last_login.empty?
+#this method checks to see if the length of self is 0, otherwise it will output false
+
+user_name.eql?("coco_11am") 
+#this method checks to see if the object has sthe same length and content as self, otherwise it will output false
+
+user_name.inspect 
+#this method retunrs a printable version of selfm enclosed in double-quotes and with specail characters escaped
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
-
+# Methods ending with "!" are also known as dangerous methods, as they modify the object that it is called on rather than a copy 
 
 
