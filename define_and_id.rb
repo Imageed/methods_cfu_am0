2c36f13ae40c072f448f20351267921281743e24
Imageed <111616894+Imageed@users.noreply.github.com>
2022-08-30 14:41:53 -0600

    Update define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,29 @@
 
 # YOU DO the rest:
 
-
+# The include? method is called on the string "Hello World"
+# The argument "Hello" is passed, the include? method checks to see if the string includes a the argument. 
+# The return value is True 
 "Hello World".include?("Hello")
 
-
+# the end_with? method is called on the string "Hello World" 
+# The argument "Hello" is passed, the end_with? method checks to see if the string ends with the argument 
+# The return value is False 
 "Hello World".end_with?("Hello")
 
-
+# the end_with? method is called on the string "Hello World"
+# The argument "Hello" is passed, the end_with? method checks to see if the string ends with the argument 
+# The return value is True 
 "Hello World".end_with?("rld")
 
-
+# The .even? method is called on the integer 12
+# no arguments are passed, .even checks to see if the integer is even
+# The return value is True =
 12.even?
 
-
+# The .next method is called on the integer 18
+# no arguments are passed, .next lists the next interger (+1) 
+# The return value is 19
 18.next
 
 
