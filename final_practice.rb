d8e3e0822d8eb03e856d16e15c38310ba8185627
Issa Abdel Mageed <imageed95@gmail.com>
2022-08-30 21:56:39 -0600

    uupdate to finals practice and methods

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,59 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
-
+def greeting
+  puts "Hello there!"
+end
+ greeting
 # What is the return value of your method?
+# The return value is nil because puts return value is nil
+
 # How many arguments did you pass your method?
+# 0 arguments were passed om this method
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
-
-
+def custom_greeting(name1)
+puts "hello there #{name1}"
+end
+ custom_greeting("issa")
 # What is the return value of your method?
+#the return value of my method "hello there issa" followed by nil
+
 # How many arguments did you pass your method?
-# What data type was your argument(s)?
+#I passed 1 argument on my method
+
+# What data type was your argument(s)
+#the data type was a string
 
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
-
-
+def square(num1)
+ return num1 * num1
+end
+puts square(10)
 
 # What is the return value of your method?
+# The return value of this method is 9
 # How many arguments did you pass your method?
+# I passed 1 argument on my method
 # What data type was your argument(s)?
+# the arguments were integers or floats
 
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+def greet_person(first, middle, last)
+  puts "hello there #{first} #{middle} #{last}"
+end
+greet_person "Issa", "Abdel", "Mageed"
 
 
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
-
--- methods_variables.rb
+++ methods_variables.rb
@@ -9,9 +9,9 @@
 #  call length on the variable, print it out
 
 chicken = "a large bird"
-puts chicken.upcase 
-puts chicken.downcase 
-puts chicken.reverse 
+puts chicken.upcase
+puts chicken.downcase
+puts chicken.reverse
 puts chicken.length
 
 
@@ -24,24 +24,25 @@ puts chicken.length
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
-user_name.start_with? ("coco")
+p user_name.start_with? ("coco")
 #this method checks to see if the variable starts with the argument
 
-last_login.empty?
+p last_login.empty?
 #this method checks to see if the length of self is 0, otherwise it will output false
 
-user_name.eql?("coco_11am") 
+p user_name.eql?("coco_11am")
 #this method checks to see if the object has sthe same length and content as self, otherwise it will output false
 
-user_name.inspect 
-#this method retunrs a printable version of selfm enclosed in double-quotes and with specail characters escaped
+p user_name.inspect
+#this method returns a printable version of self enclosed in double-quotes and with specail characters escaped
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
-# Methods ending with "!" are also known as dangerous methods, as they modify the object that it is called on rather than a copy 
-
-
+# Methods ending with "!" are also known as dangerous methods, as they modify the object that it is called on rather than a copy
+chicken = "a tasty snack"
+chicken.upcase!
+p chicken
